b6be70b5d7945e8b18ea29423bb66430cbe757d8
FerDev12 <fernando.mu.dev@gmail.com>
2024-03-11 20:46:24 -0600

    test: actions test secret

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,18 +1,21 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-// import ws from 'ws';
+import ws from 'ws';
 import * as schema from '@/models';
 
 const { NODE_ENV: ENV, NEON_DB_URL: DB_URL } = process.env;
 
-// if (ENV === 'test') {
-//   neonConfig.webSocketConstructor = ws;
-// }
+if (ENV === 'test') {
+  neonConfig.webSocketConstructor = ws;
+}
+
+const max = ENV === 'test' ? 1 : 20;
+console.log({ test_secret: process.env.TEST_SECRET });
 
 export const pool = new Pool({
   connectionString: DB_URL,
   connectionTimeoutMillis: 2000,
-  max: 20,
+  max,
 });
 
 export const db = drizzle(pool, { schema });
